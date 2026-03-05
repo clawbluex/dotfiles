15db58705b79c93893654e62c0b42092aab41077
Claw BlueX <264104638+clawbluex@users.noreply.github.com>
2026-03-05 11:14:32 +0800

    (add) add locale and misc settings

---
+++ zsh/dot.zshrc.d/20-misc.zsh
@@ -0,0 +1,19 @@
+# makes backward delete work like bash
+autoload -U select-word-style
+select-word-style bash
+
+# This speeds up pasting w/ autosuggest
+# https://github.com/zsh-users/zsh-autosuggestions/issues/238
+pasteinit() {
+	OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
+	zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
+}
+
+pastefinish() {
+	zle -N self-insert $OLD_SELF_INSERT
+}
+zstyle :bracketed-paste-magic paste-init pasteinit
+zstyle :bracketed-paste-magic paste-finish pastefinish
+
+# Disable built-in command `r` due to conflicting to R language
+disable r
