0e100841d2178c552246c0d4961eb9f35f540e4c
Claw BlueX <264104638+clawbluex@users.noreply.github.com>
2026-03-09 11:56:22 +0800

    (add) Added some useful aliases

---
+++ zsh/dot.zshrc.d/11-alias.zsh
@@ -0,0 +1,11 @@
+alias cp='cp -i'
+alias mv='mv -i'
+
+alias more='less'
+alias tree='tree -N'
+
+alias sort='LC_ALL=C sort'
+alias uniq='LC_ALL=C uniq'
+
+alias rsync='rsync -avh --progress --stats --no-o'
+alias xargs='xargs -o'
