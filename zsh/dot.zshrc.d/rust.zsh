7c4fd34e209a2b6951d7f50eb24f0215f1be90ec
Claw BlueX <264104638+clawbluex@users.noreply.github.com>
2026-07-03 16:02:48 +0800

    (chore) update for rust
    Homebrew rustup formula no logger provides `rustup-init`.

--- zsh/dot.zshrc.d/rust.zsh
+++ zsh/dot.zshrc.d/rust.zsh
@@ -1,3 +1,13 @@
+HOMEBREW_PREFIX=${HOMEBREW_PREFIX:-/usr/local}
+
+# brew install rustup
+if [ -d "${HOMEBREW_PREFIX}/opt/rustup" ]; then
+	case ":$PATH:" in
+		*:"${HOMEBREW_PREFIX}/opt/rustup/bin":*) ;;
+		*) path=("${HOMEBREW_PREFIX}/opt/rustup/bin" $path) ;;
+	esac
+fi
+
 # add ~/.cargo/bin to path
 [[ -s "${HOME}/.cargo/env" ]] && source "${HOME}/.cargo/env"
 if [ -d "${HOME}/.cargo/bin" ]; then
